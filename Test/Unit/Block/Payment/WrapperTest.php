83bc8608c3a21f0c4b95984b2c38ba1ae9102f08
Tiago Sampaio <tiago@tiagosampaio.com>
2018-11-24 19:02:24 -0200

    Improvements to unit testing.

--- Test/Unit/Block/Payment/WrapperTest.php
+++ Test/Unit/Block/Payment/WrapperTest.php
@@ -7,5 +7,28 @@ use ZPay\Standard\Test\Unit\Block\BlockAbstract;
 
 class WrapperTest extends BlockAbstract
 {
-
+    
+    /**
+     * @var Wrapper
+     */
+    private $block;
+    
+    protected function setUp()
+    {
+        $this->block = $this->getObject(\ZPay\Standard\Block\Payment\Wrapper::class, [
+        ]);
+    }
+    
+    public function testConvertDate()
+    {
+        $input    = 'November 24th 2018 00:00:00';
+        $expected = '2018-11-24 00:00:00';
+        
+        $this->block
+            ->getTimezone()
+            ->method('date')
+            ->willReturn(new \DateTime($input));
+        
+        $this->assertEquals($expected, $this->block->convertDate($input));
+    }
 }
