e0a5c1aec21c63528d2521b84a9b20987555efa2
Tiago Sampaio <tiago@tiagosampaio.com>
2019-01-10 12:01:55 -0200

    Updated the copyright.

--- registration.php
+++ registration.php
@@ -1,6 +1,14 @@
 <?php
 /**
+ * ZPay Payment Gateway
+ *
+ * @category ZPay
+ * @package ZPay\Standard
  * @author Tiago Sampaio <tiago@tiagosampaio.com>
+ * @link https://github.com/tiagosampaio
+ * @link https://tiagosampaio.com
+ *
+ * Copyright (c) 2019.
  */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
