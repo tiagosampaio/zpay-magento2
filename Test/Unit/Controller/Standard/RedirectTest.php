6d7e5f6510aff1b4766da0c2f04a2acc9be1909b
Tiago Sampaio <tiago@tiagosampaio.com>
2019-01-10 11:57:07 -0200

    Updated the copyright.

--- Test/Unit/Controller/Standard/RedirectTest.php
+++ Test/Unit/Controller/Standard/RedirectTest.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * ZPay Payment Gateway
+ *
+ * @category ZPay
+ * @package ZPay\Standard
+ * @author Tiago Sampaio <tiago@tiagosampaio.com>
+ * @link https://github.com/tiagosampaio
+ * @link https://tiagosampaio.com
+ *
+ * Copyright (c) 2019.
+ */
+
+declare(strict_types = 1);
 
 namespace ZPay\Standard\Test\Unit\Controller\Standard;
 
